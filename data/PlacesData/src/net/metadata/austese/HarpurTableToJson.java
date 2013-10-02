7ae470d72c9bc658093eda7f28907851911aabdf
Anna Gerber <anna.m.gerber@gmail.com>
2013-10-02 15:06:45 +1000

    Updated import script for HCA table

--- data/PlacesData/src/net/metadata/austese/HarpurTableToJson.java
+++ data/PlacesData/src/net/metadata/austese/HarpurTableToJson.java
@@ -18,10 +18,15 @@ public class HarpurTableToJson {
   public static final String WORK_COLLECTION = "works";
   public static final String VERSION_COLLECTION = "versions";
   public static final String ARTEFACT_COLLECTION = "artefacts";
+  public static final String EVENT_COLLECTION = "events";
+  public static final String MAPPING_COLLECTION = "mappings";
+  private static String projectId;
+  private static ArrayList<HashMap<String,String>> eventList = new ArrayList<HashMap<String,String>>();
   private static ArrayList<HashMap<String,String>> artefactList = new ArrayList<HashMap<String,String>>();
   private static ArrayList<HashMap<String,String>> versionList = new ArrayList<HashMap<String,String>>();
   private static ArrayList<HashMap<String,String>> workList = new ArrayList<HashMap<String,String>>();
-  
+  private static ArrayList<HashMap<String,String>> mappingList = new ArrayList<HashMap<String,String>>();
+  private static HashMap<String,ArrayList<String>> artefactParts = new HashMap<String,ArrayList<String>>();
   /*
    * Transform Harpur table (copied to CSV from Word Table by pasting into Excel) into JSON
    */
@@ -32,67 +37,113 @@ public class HarpurTableToJson {
         props.load(new FileInputStream("transform.properties"));
         String outputFileName = props.getProperty("harpurOutput");
         String inputFileName = props.getProperty("harpurInput");
-        
+        projectId = props.getProperty("harpurProjectId");
         CSVReader reader = new CSVReader(new FileReader(inputFileName));
         String [] nextLine;
-        //Work no.,Work title,Version no.,Date,First line of version,Title of version,Source,Bib. details
+      //Work no.,Work title,Version no.,Date,First line of version,Title of version,Source,Bib. details
+      //0 Work no., 1 Work title,2 Version no.,3 Year, 4 Date,5 First line of version,6 Blank, 7 Title of version,8 Source,9 Page no, 10 Notes, 11 Series, 12 Version no, 13 lines, 14 ms, 15 url
         String versTitle, workTitle="";
         HashMap<String,String> currentWork = null;
         ArrayList<String> currentVersions = new ArrayList<String>();
+        boolean firstLine = true;
+        int rowNumber = 0;
         while ((nextLine = reader.readNext()) != null) {
-
-            // If this line represents a new work, create a work record
-            if (!nextLine[0].equals("")){
-                // add list of versions accumulated so far to previous work and then reset
-                if (currentVersions.size() != 0 && currentWork != null){
-                    StringBuffer cVers = new StringBuffer();
-                    cVers.append("[");
-                    Iterator<String> it = currentVersions.iterator();
-                    while(it.hasNext()){
-                        cVers.append("\"" + it.next() + "\"");
-                        if (it.hasNext()){
-                            cVers.append(", ");
+            if (!firstLine){
+                rowNumber++;
+                // If this line represents a new work, create a work record
+                if (!nextLine[0].equals("")){
+                    // add list of versions accumulated so far to previous work and then reset
+                    if (currentVersions.size() != 0 && currentWork != null){
+                        StringBuffer cVers = new StringBuffer();
+                        cVers.append("[");
+                        Iterator<String> it = currentVersions.iterator();
+                        while(it.hasNext()){
+                            cVers.append("\"" + it.next() + "\"");
+                            if (it.hasNext()){
+                                cVers.append(", ");
+                            }
+                        }
+                        cVers.append("]");
+                        // for each in currentVersions add new ObjectId(currentVersions[i]) to cVers array
+                        currentWork.put("versions", cVers.toString());
+                        currentVersions.clear();
+                    }
+                    currentWork = new HashMap<String,String>();
+                    currentWork.put("name",nextLine[0]);
+                    workTitle = nextLine[1];
+                    currentWork.put("workTitle", replaceStupidCharacters(workTitle));
+                    workList.add(currentWork);
+                }
+                if (nextLine[2] == null || !nextLine[2].equals("")){
+                    // Create a version  (don't worry about issues, Harpur project only has to deal with one issue per version)
+                    HashMap<String,String> v = new HashMap<String,String>();
+                    String versionId = new ObjectId().toString();
+                    v.put("importRowNumber", rowNumber + "");
+                    v.put("_id", "{ \"$oid\" : \"" + versionId + "\"}");
+                    // push version id to currentVersions for later linking from work
+                    currentVersions.add(versionId);
+                    v.put("name",nextLine[2]);
+                    v.put("date", (!nextLine[4].equals("")? nextLine[4] + " ": "") + nextLine[3]);
+                    v.put("lines", nextLine[13]);
+                    v.put("firstLine", replaceStupidCharacters(nextLine[5]));
+                    versTitle = nextLine[7];
+                    if (versTitle.startsWith("^")){
+                        versTitle = workTitle;
+                    }
+                    v.put("versionTitle", replaceStupidCharacters(versTitle));
+                    versionList.add(v);
+                    
+                    if (nextLine[2] == null || !nextLine[9].equals("")){
+                        // Create an Artefact (part) for each version
+                        //8 Source,9 Page no, 10 Notes, 11 Series, 12 Version no, 13 lines, 14 ms, 15 url
+                        HashMap<String,String> a = new HashMap<String,String>();
+                        String artefactId = new ObjectId().toString();
+                        String source = nextLine[8].trim();
+                        String pageNumbers = nextLine[9].trim();
+                        a.put("importRowNumber", rowNumber + "");
+                        a.put("_id","{ \"$oid\" : \"" + artefactId + "\"}");
+                        a.put("source", source + (!pageNumbers.equals("") ? " p" + pageNumbers: ""));
+                        a.put("pageNumbers", pageNumbers);
+                        a.put("description", nextLine[10]); // actually notes field
+                        a.put("series", nextLine[11]);
+                        a.put("date", (!nextLine[4].equals("")? nextLine[4] + " ": "") + nextLine[3]);
+                        artefactList.add(a);
+                        
+                        // add source,artefact id to artefact parts map
+                        if (!source.equals("")){
+                            if (artefactParts.containsKey(source)){
+                                artefactParts.get(source).add(artefactId);
+                            } else {
+                                ArrayList<String> parts = new ArrayList<String>();
+                                parts.add(artefactId);
+                                artefactParts.put(source, parts);
+                            }
+                        }
+                        // Create temporary mappings that will be used later to match uploaded images with artefacts
+                        HashMap<String,String> t = new HashMap<String,String>();
+                        t.put("artefact",artefactId);
+                        t.put("version", versionId);
+                        t.put("ms", nextLine[14]);
+                        t.put("imageUrl", nextLine[15]);
+                        mappingList.add(t);
+                        
+                        // generate link from version to artefact
+                        v.put("artefacts", "[\"" + artefactId + "\"]");
+                        
+                        if (!nextLine[0].equals("")){
+                            // composition event for each work
+                            HashMap<String,String> e = new HashMap<String,String>();
+                            e.put("artefacts", "[\"" + artefactId + "\"]");
+                            e.put("startDate", nextLine[3]);
+                            e.put("eventType", "composition");
+                            e.put("name", "\\\"" + nextLine[1] + "\\\" composed");
+                            eventList.add(e);
                         }
                     }
-                    cVers.append("]");
-                    // for each in currentVersions add new ObjectId(currentVersions[i]) to cVers array
-                    currentWork.put("versions", cVers.toString());
-                    currentVersions.clear();
                 }
-                currentWork = new HashMap<String,String>();
-                currentWork.put("name",nextLine[0]);
-                workTitle = nextLine[1];
-                currentWork.put("workTitle", replaceStupidCharacters(workTitle));
-                workList.add(currentWork);
+            } else {
+                firstLine = false;
             }
-            
-            // Create a version  (don't worry about issues, Harpur project only has to deal with one issue per version)
-            HashMap<String,String> v = new HashMap<String,String>();
-            String versionId = new ObjectId().toString();
-            v.put("_id", "ObjectId('" + versionId + "')");
-            // push version id to currentVersions for later linking from work
-            currentVersions.add(versionId);
-            v.put("name",nextLine[2]);
-            v.put("date", nextLine[3]);
-            v.put("firstLine", replaceStupidCharacters(nextLine[4]));
-            versTitle = nextLine[5];
-            if (versTitle.startsWith("^")){
-                versTitle = workTitle;
-            }
-            v.put("versionTitle", replaceStupidCharacters(versTitle));
-            versionList.add(v);
-            
-            // Create an Artefact for each line
-            HashMap<String,String> a = new HashMap<String,String>();
-            String artefactId = new ObjectId().toString();
-            a.put("_id","ObjectId('" + artefactId + "')");
-            a.put("source",nextLine[6]);
-            a.put("bibDetails", nextLine[7]);
-            a.put("date", nextLine[3]);
-            artefactList.add(a);
-            
-            // generate link from version to artefact
-            v.put("artefacts", "[\"" + artefactId + "\"]");
         }
         // versions for last work
         if (currentVersions.size() != 0 && currentWork != null){
@@ -109,48 +160,99 @@ public class HarpurTableToJson {
             // for each in currentVersions add new ObjectId(currentVersions[i]) to cVers array
             currentWork.put("versions", cVers.toString());
         }
-       File outputFile = new File(outputFileName);
+        
+        // iterate over artefact parts, if number of parts is more than 1, add an artefact to represent the source
+       for (String k : artefactParts.keySet()) {
+           ArrayList<String> parts = artefactParts.get(k);
+           if (parts.size() > 1){
+               String artefactsString = "[";
+               for (int j=0; j<parts.size();j++){
+                   artefactsString += "\"" + parts.get(j) + "\"";
+                   if (j < (parts.size() -1)) {
+                       artefactsString += ",";
+                   }
+               }
+               artefactsString += "]";
+               HashMap<String,String> a = new HashMap<String,String>();
+               String artefactId = new ObjectId().toString();
+               a.put("_id","{ \"$oid\" : \"" + artefactId + "\"}");
+               a.put("source",k);
+               a.put("artefacts", artefactsString);
+               artefactList.add(a);
+           }
+       }
+        // write works to file
+       File outputFile = new File(outputFileName + WORK_COLLECTION + ".json");
        outputFile.delete();
+       outputFile = new File(outputFileName + ARTEFACT_COLLECTION + ".json");
+       outputFile.delete();
+       outputFile = new File(outputFileName + VERSION_COLLECTION + ".json");
+       outputFile.delete();
+       outputFile = new File(outputFileName + MAPPING_COLLECTION + ".json");
+       outputFile.delete();
+       outputFile = new File(outputFileName + EVENT_COLLECTION + ".json");
+       outputFile.delete();
+       
        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
-               new FileOutputStream(outputFileName), "UTF-8"));
-       bw.write("db." + WORK_COLLECTION + ".remove();\n\n");
+               new FileOutputStream(outputFileName + WORK_COLLECTION + ".json"), "UTF-8"));
        for (HashMap<String,String> w : workList) {
-         bw.write("db." + WORK_COLLECTION + ".insert(" + mapToString(w) + ");\n");
-       }
-       bw.write("db." + VERSION_COLLECTION + ".remove();\n\n");
-       for (HashMap<String,String> v : versionList) {
-         bw.write("db." + VERSION_COLLECTION + ".insert(" + mapToString(v) + ");\n");
-       }
-       bw.write("db." + ARTEFACT_COLLECTION + ".remove();\n\n");
-       for (HashMap<String,String> a : artefactList) {
-         bw.write("db." + ARTEFACT_COLLECTION + ".insert(" + mapToString(a) + ");\n");
-       }
-       //bw.write("\ndb." + COLLECTION_NAME + ".ensureIndex({name: 1});");
+           bw.write(mapToString(w) + "\n");
+         }
        bw.close();
+        
+       bw = new BufferedWriter(new OutputStreamWriter(
+               new FileOutputStream(outputFileName + VERSION_COLLECTION + ".json"), "UTF-8"));
+       for (HashMap<String,String> v : versionList) {
+           bw.write(mapToString(v) + "\n");
+       }
+       bw.close();
+       
+       bw = new BufferedWriter(new OutputStreamWriter(
+               new FileOutputStream(outputFileName + ARTEFACT_COLLECTION + ".json"), "UTF-8"));
+       for (HashMap<String,String> a : artefactList) {
+           bw.write(mapToString(a) + "\n");
+       }
+       bw.close();
+       
+       bw = new BufferedWriter(new OutputStreamWriter(
+               new FileOutputStream(outputFileName + MAPPING_COLLECTION + ".json"), "UTF-8"));
+       for (HashMap<String,String> t : mappingList) {
+           bw.write(mapToString(t) + "\n");
+       }
+       bw.close();
+       
+       bw = new BufferedWriter(new OutputStreamWriter(
+               new FileOutputStream(outputFileName + EVENT_COLLECTION + ".json"), "UTF-8"));
+       for (HashMap<String,String> e : eventList) {
+           bw.write(mapToString(e) + "\n");
+       }
+       bw.close();
+
      } catch (IOException e) {
        e.printStackTrace();
      }
   }
   private static String replaceStupidCharacters(String input){
-      return input.replaceAll("Ò", "\"\"").replaceAll("Ó", "\"").replaceAll("Ñ", "-").replaceAll("Õ", "'").replaceAll("Ô", "'");
+      return input.replaceAll("Ò", "\\\\\"").replaceAll("Ó", "\\\\\"").replaceAll("Ñ", "-").replaceAll("Õ", "'").replaceAll("Ô", "'");
   }
   public static String mapToString(HashMap<String,String> m) {
+      
     StringBuffer revisionsBuffer= new StringBuffer();
     StringBuffer metadataBuffer = new StringBuffer();
     String idProp = "";
-    revisionsBuffer.append("{\"project\": \"Harpur\", \"_revisions\":[");
-    revisionsBuffer.append("{");
+    revisionsBuffer.append("{\"_revisions\":[");
+    revisionsBuffer.append("{\"project\": \"" + projectId + "\",");
     Iterator<Entry<String,String>> entries = m.entrySet().iterator();
     while (entries.hasNext()) {
        Entry<String,String> entry = entries.next();
        String key = entry.getKey();
        String value = entry.getValue();
-       if (!value.startsWith("[\"") && !value.startsWith("ObjectId")){
+       if (!value.startsWith("[\"") && !value.startsWith("{")){
            value = "\"" + value + "\"";
        }
        if (!key.equals("_id")){
-           revisionsBuffer.append(key + ": " + value);
-           metadataBuffer.append(key + ": " + value);
+           revisionsBuffer.append("\"" + key + "\": " + value);
+           metadataBuffer.append("\"" + key + "\": " + value);
            if (entries.hasNext()){
                revisionsBuffer.append(", ");
                metadataBuffer.append(", ");
@@ -159,7 +261,7 @@ public class HarpurTableToJson {
            idProp = ", \"_id\": " + value;
        }
     }
-    revisionsBuffer.append("}], \"metadata\":{");
+    revisionsBuffer.append("}], \"metadata\":{\"project\":\"" + projectId + "\",");
     revisionsBuffer.append(metadataBuffer.toString());
     revisionsBuffer.append("}");
     revisionsBuffer.append(idProp);
